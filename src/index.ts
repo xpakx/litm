c6f5bc6cf7543534ee3b0886018ccb13847df4a0
xpakx <55586470+xpakx@users.noreply.github.com>
2026-02-23 20:44:39 +0100

    refactor: extract windows

--- src/index.ts
+++ src/index.ts
@@ -11,45 +11,60 @@ const clockService = (ctx: WindowContext) => {
 }
 
 
+function musicWindow(x: number, y: number, id: string): any {
+	return {
+		title: 'Music',
+		x: x,
+		y: y,
+		width: 420, 
+		height: 280,
+		template: `
+		<iframe 
+		style="width: 100%; height: 100%;"
+		src="https://www.youtube.com/embed/${id}" 
+			frameborder="0" 
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+		allowfullscreen>
+		</iframe>
+		`,
+		services: []
+	}
+}
+
+function clockWindow(x: number, y: number) {
+	return {
+		title: 'Clock',
+		x: x,
+		y: y,
+		width: 200,
+		height: 100,
+		template: `<div id="clock-${Date.now()}" style="font-size:20px; text-align:center">...</div>`,
+		services: [clockService]
+	}
+}
+
+function basicWindow(x: number, y: number, text: string): any {
+	return {
+		title: 'Basic Window',
+		x: x,
+		y: y,
+		width: 250, 
+		height: 120,
+		services: [],
+		template: `
+		<p style="text-align:center; margin:0; color:#666">
+		${text}
+		</p>`
+	}
+}
+
+
 (() => {
     const app = new App("app");
 
-    app.register({
-	    title: 'Basic Window',
-	    x: 50,
-	    y: 50,
-	    width: 250, 
-	    height: 120,
-	    services: [],
-	    template: `<p style="text-align:center; margin:0; color:#666">Hello World</p>`
-    });
+    app.register(basicWindow(50, 50, "Hello world"));
 
-    app.register({
-        title: 'Music',
-        x: 720,
-	y: 50,
-        width: 420, 
-	height: 280,
-        template: `
-                <iframe 
-                    style="width: 100%; height: 100%;"
-                    src="https://www.youtube.com/embed/krGs2V3Vk3w" 
-                    frameborder="0" 
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                    allowfullscreen>
-                </iframe>
-        `,
-        services: []
-    });
-
-    app.register({
-	    title: 'Clock',
-	    x: 10,
-	    y: 200,
-	    width: 200,
-	    height: 100,
-	    template: `<div id="clock-${Date.now()}" style="font-size:20px; text-align:center">...</div>`,
-	    services: [clockService]
-    });
+    app.register(musicWindow(720, 50, "krGs2V3Vk3w"));
+    app.register(clockWindow(10, 200));
 
 })();
