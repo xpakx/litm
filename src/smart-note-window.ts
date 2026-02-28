a4cc983dd297375918702c894440ec8104539e18
xpakx <55586470+xpakx@users.noreply.github.com>
2026-02-28 22:48:34 +0100

    fix: fix bug with transforming tags at the beginning of text node

--- src/smart-note-window.ts
+++ src/smart-note-window.ts
@@ -55,7 +55,7 @@ class SmartNoteService implements Service {
 		const selection = window.getSelection();
 
 		const matchIndex = match.index;
-		if (!matchIndex) return;
+		if (matchIndex === undefined) return;
 		const fullMatchStr = match[0];
 
 		let startOffset = matchIndex;
