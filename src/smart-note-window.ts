2d676f585711838dbd10e24c2f8291c8b3edbe67
xpakx <55586470+xpakx@users.noreply.github.com>
2026-02-28 22:21:37 +0100

    feat: detect tags in smart editor

--- src/smart-note-window.ts
+++ src/smart-note-window.ts
@@ -17,7 +17,34 @@ class SmartNoteService implements Service {
 		this.editor = document.getElementById('smart-editor') as HTMLElement;
 
 		this.smartNoteEditor.addEventListener('click', () => this.focusEditor());
+		this.editor.addEventListener('input', (e: Event) => this.onInput(e as InputEvent))
 	}
+
+	onInput(_event: InputEvent) {
+            const selection = window.getSelection();
+            if (!selection || !selection.rangeCount) return;
+
+            const range = selection.getRangeAt(0);
+            const node = range.endContainer;
+            if (node.nodeType !== Node.TEXT_NODE) return;
+
+            const text = node.textContent;
+	    if (!text) return;
+            
+	    // TODO: on paste, on comma, etc.
+            if (!text.endsWith('\u00A0') && !text.endsWith(' ')) return;
+            const regex = /(?:^|\s|[\u00A0])([+-])([a-zA-Z0-9_]+)(?:-(\d+))?[\s\u00A0]$/;
+            
+            const match = text.match(regex);
+
+            if (match) {
+                const fullMatch = match[0];
+                const type = match[1];
+                const label = match[2];
+                const value = match[3];
+		console.log(fullMatch);
+            }
+        }
 }
 
 export function smartNoteWindow(x: number, y: number): any {
