493feb4df893b34cabe9fe79195585a6cc634eaa
xpakx <55586470+xpakx@users.noreply.github.com>
2026-02-28 22:35:09 +0100

    feat: insert new tags into smart text window

--- src/smart-note-window.ts
+++ src/smart-note-window.ts
@@ -39,11 +39,62 @@ class SmartNoteService implements Service {
 
             if (match) {
                 const fullMatch = match[0];
-                const type = match[1];
+                const positive = match[1] != '-';
                 const label = match[2];
                 const value = match[3];
 		console.log(fullMatch);
+		console.log(positive, label, value);
+		if (!label) return;
+                this.createSmartTag(node, match, positive, label, value);
+
             }
+        }
+
+        createSmartTag(textNode: Node, match: RegExpMatchArray, positive: boolean, label: string, value?: string) {
+		const range = document.createRange();
+		const selection = window.getSelection();
+
+		const matchIndex = match.index;
+		if (!matchIndex) return;
+		const fullMatchStr = match[0];
+
+		let startOffset = matchIndex;
+		if (fullMatchStr.match(/^[\s\u00A0]/)) {
+			startOffset += 1; 
+		}
+
+		range.setStart(textNode, startOffset);
+		range.setEnd(textNode, matchIndex + fullMatchStr.length);
+		range.deleteContents();
+
+		const wrapper = document.createElement('span');
+		wrapper.contentEditable = "false";
+		wrapper.className = "smart-tag-wrapper";
+
+		let styleClass = "style-pos";
+		if (value) styleClass = "style-status";
+		else if (!positive) styleClass = "style-neg";
+
+		wrapper.classList.add(styleClass);
+
+		const textSpan = document.createElement('span');
+		textSpan.className = "marker-text";
+		textSpan.innerText = label;
+		wrapper.appendChild(textSpan);
+
+		range.insertNode(wrapper);
+
+		const spaceNode = document.createTextNode('\u00A0');
+		range.setStartAfter(wrapper);
+		range.setEndAfter(wrapper);
+		range.insertNode(spaceNode);
+
+		range.setStartAfter(spaceNode);
+		range.setEndAfter(spaceNode);
+
+		if (!selection) return;
+		selection.removeAllRanges();
+		selection.addRange(range);
         }
 }
 
