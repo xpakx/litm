440bf9683528f2b48b135160b0dc26b1b16da4a9
xpakx <55586470+xpakx@users.noreply.github.com>
2026-04-02 23:58:37 +0200

    feat: create base elem container

---
+++ src/core/container.ts
@@ -0,0 +1,59 @@
+import type { PanelSettings } from "./app.js";
+import type { HTMLComponent } from "./html-component.js";
+
+interface Card {
+	component: HTMLElement | HTMLComponent;
+	settings: PanelSettings | undefined;
+}
+
+export class Container {
+	_zone: HTMLElement;
+	_container: HTMLElement;
+	_cards: Card[] = [];
+
+	constructor(zone: HTMLElement) {
+		this._zone = zone;
+
+		let container = this._zone.querySelector('.app-panel-container') as HTMLElement;
+		if (container) {
+			this._container = container;
+		} else {
+			this._container = document.createElement('div');
+			this._container.className = 'app-panel-container';
+			this._zone.insertBefore(this._container, zone.firstChild);
+		}
+	}
+
+	addElement(component: HTMLElement | HTMLComponent, settings?: PanelSettings) {
+		this._container.appendChild(component);
+		this._cards.push({
+			component: component,
+			settings: settings,
+		});
+	}
+
+	removeElement(index: number): HTMLElement | HTMLComponent | null {
+		const card = this._cards[index];
+		if (!card) return null;
+
+		if (card.component instanceof Node) this._container.removeChild(card.component);
+		this._cards.splice(index, 1);
+
+		return card.component;
+	}
+
+	killElement(index: number) {
+		const card = this._cards[index];
+		if (!card) return;
+
+		if (card.component instanceof Node) this._container.removeChild(card.component);
+		this._cards.splice(index, 1);
+
+		if ('destroy' in card.component && card.component.permanent) card.component.destroy();
+
+		const services = card.settings?.services ?? [];
+		services.forEach((s) => {
+			if ('destroy' in s) s.destroy(card.component);
+		});
+	}
+}
